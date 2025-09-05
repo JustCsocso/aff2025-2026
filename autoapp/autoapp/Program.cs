5901fb36fec9bef1bf4d10019e2d69bc2e34a108
unknown <pinter.csongor.21@ady-nagyatad.hu>
2025-09-05 13:15:11 +0200

    dw

---
+++ autoapp/autoapp/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using autoapp;
+
+string[] olvas = File.ReadAllLines("autok.csv"); 
+List<Auto> adatok=new List<Auto>();
+
+foreach (string line in olvas.Skip(1))
+{
+    adatok.Add(new Auto(line));
+}
+
+Console.WriteLine("5.feladat: {0} auto volt ",adatok.Count());
+
+int darab = 0;
+int eladott = 0;
+foreach(Auto e in adatok)
+{
+    if (e.eladott!=0)
+    {
+        eladott+= e.eladott;
+        darab++;
+    }
+}
+
+double atlag = Math.Round((double)eladott / darab, 1);
+Console.WriteLine(atlag);
+
+Console.WriteLine("7.feladat: Az elmúlt 5 évben gyártott autók: ");
+
+foreach(Auto e in adatok)
+{
+    if (e.gyartev)
+}
